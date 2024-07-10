55abdec1f32779b40ef340ecb37a45b41336235b
KaiJin <sangwell@126.com>
2024-07-10 22:11:48 +0800

    add component

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps(['title', 'value'])
+</script>
+
+<template>
+  <div class="border">
+    <div class="name">
+      {{ title }}
+    </div>
+    <div class="value">
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.border {
+  height: 140px;
+  border: 1px solid #cccccc;
+  margin: 0 10px;
+  border-radius: 10px;
+
+  .name {
+    height: 40px;
+    padding: 0 12px;
+    font-size: 16px;
+    line-height: 40px;
+  }
+
+  .value {
+    height: 100px;
+    text-align: right;
+    line-height: 100px;
+    font-size: 40px;
+    font-weight: bold;
+    padding: 0 20px;
+    color: green;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,51 @@
 <script setup lang="ts">
-import WelcomeList from '../components/WelcomeList.vue'
+import { onMounted, onUnmounted } from 'vue'
+import { ref } from 'vue'
+import Card from '../components/Card.vue'
+// 生命周期
+onMounted(() => {
+  console.log(`the component is now mounted.`)
+})
+
+onUnmounted(() => {
+  console.log(`the component is now unmounted.`)
+})
+
+const items = ref([
+  { name: '日期', value: '星期一' },
+  { name: '温度', value: '31°C' },
+  { name: '天气', value: '晴' },
+  { name: '气压', value: '101kPa' },
+  { name: '风速', value: '10m/s' }
+])
 </script>
 
 <template>
-  <h2>首页</h2>
+  <main>
+    <h2>首页</h2>
+    <div class="box-layout">
+      <div class="box-item" v-for="item in items">
+        <Card :title="item.name"
+              :value="item.value" />
+      </div>
+    </div>
+
+  </main>
+
 </template>
+
+<style scoped>
+main {
+  padding: 10px;
+}
+.box-layout {
+  display: flex;
+}
+
+.box-item {
+  width: 25%;
+  height: 100px;
+  border: none;
+  flex: 1;
+}
+</style>
